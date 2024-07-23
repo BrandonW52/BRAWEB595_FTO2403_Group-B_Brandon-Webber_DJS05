e9e6f3bc311f8247308d667ebc9247e3ac36e368
BrandonW52 <157122603+BrandonW52@users.noreply.github.com>
2024-07-23 13:45:14 +0200

    Created Initial state

---
+++ state.js
@@ -0,0 +1,10 @@
+export const initialState = {
+  // empty array to add our groceries to
+  value: 0,
+};
+
+// Action types
+// P.S. I don't belive they go here, just for this project
+export const ADD_ONE = "ADD_ONE";
+export const REMOVE_ONE = "REMOVE_ONE";
+export const CLEAR_TALLY = "CLEAR_TALLY";
